5fbba7ce1ee92e8a43e34f3986d6a4af6e6250b2
ITTM <asinmaxim14@gmail.com>
2025-11-14 16:25:04 +0700

    Update DvD Window (2).cpp

--- DvD Window (2).cpp
+++ DvD Window (2).cpp
@@ -1,82 +1,186 @@
+//DvD Window (3)
 #include <Windows.h>
-#include <iostream>
 #include <vector>
+#include <set>
+#include <thread>
+#include <chrono>
 
-BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam) {
-    std::vector<HWND>* windows = reinterpret_cast<std::vector<HWND>*>(lParam);
-
-    if (IsWindowVisible(hwnd) && GetParent(hwnd) == NULL) {
-        windows->push_back(hwnd);
-    }
-
-    return TRUE;
-}
-
-int main() {
+class WindowManager {
+private:
     std::vector<HWND> windows;
-    EnumWindows(EnumWindowsProc, reinterpret_cast<LPARAM>(&windows));
-
     std::vector<POINT> originalPositions;
     std::vector<SIZE> originalSizes;
-
-    for (HWND hwnd : windows) {
-        RECT rect;
-        GetWindowRect(hwnd, &rect);
-        originalPositions.push_back({ rect.left, rect.top });
-        originalSizes.push_back({ rect.right - rect.left, rect.bottom - rect.top });
-    }
-
-    int screenW = GetSystemMetrics(SM_CXSCREEN);
-    int screenH = GetSystemMetrics(SM_CYSCREEN);
-
     std::vector<POINT> currentPositions;
     std::vector<POINT> directions;
+    std::set<HWND> processedWindows;
+    bool running;
+    int screenW, screenH;
 
-    for (size_t i = 0; i < windows.size(); i++) {
-        currentPositions.push_back(originalPositions[i]);
+    CRITICAL_SECTION cs;
 
-        POINT dir;
-        dir.x = 3;
-        dir.y = 3;
-
-        if (rand() % 2) dir.x = -dir.x;
-        if (rand() % 2) dir.y = -dir.y;
-
-        directions.push_back(dir);
+public:
+    WindowManager() : running(true) {
+        InitializeCriticalSection(&cs);
+        screenW = GetSystemMetrics(SM_CXSCREEN);
+        screenH = GetSystemMetrics(SM_CYSCREEN);
     }
 
-    while (true) {
-        for (size_t i = 0; i < windows.size(); i++) {
-            currentPositions[i].x += directions[i].x;
-            currentPositions[i].y += directions[i].y;
+    ~WindowManager() {
+        DeleteCriticalSection(&cs);
+    }
 
-            if (currentPositions[i].x <= 0) {
-                currentPositions[i].x = 0;
-                directions[i].x = -directions[i].x;
-            }
-            else if (currentPositions[i].x + originalSizes[i].cx >= screenW) {
-                currentPositions[i].x = screenW - originalSizes[i].cx;
-                directions[i].x = -directions[i].x;
-            }
+    static BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam) {
+        WindowManager* manager = reinterpret_cast<WindowManager*>(lParam);
+        return manager->AddWindow(hwnd);
+    }
 
-            if (currentPositions[i].y <= 0) {
-                currentPositions[i].y = 0;
-                directions[i].y = -directions[i].y;
-            }
-            else if (currentPositions[i].y + originalSizes[i].cy >= screenH) {
-                currentPositions[i].y = screenH - originalSizes[i].cy;
-                directions[i].y = -directions[i].y;
-            }
+    BOOL AddWindow(HWND hwnd) {
+        EnterCriticalSection(&cs);
 
-            SetWindowPos(windows[i], 0,
-                currentPositions[i].x,
-                currentPositions[i].y,
-                originalSizes[i].cx,
-                originalSizes[i].cy,
-                SWP_NOZORDER | SWP_NOACTIVATE);
+        if (processedWindows.find(hwnd) != processedWindows.end()) {
+            LeaveCriticalSection(&cs);
+            return TRUE;
         }
-        Sleep(10);
+
+        if (IsWindowVisible(hwnd) && GetParent(hwnd) == NULL) {
+            if (!IsSystemWindow(hwnd)) {
+                windows.push_back(hwnd);
+
+                RECT rect;
+                GetWindowRect(hwnd, &rect);
+                originalPositions.push_back({ rect.left, rect.top });
+                originalSizes.push_back({ rect.right - rect.left, rect.bottom - rect.top });
+                currentPositions.push_back({ rect.left, rect.top });
+
+                POINT dir;
+                dir.x = 3 + (rand() % 5);
+                dir.y = 3 + (rand() % 5);
+
+                if (rand() % 2) dir.x = -dir.x;
+                if (rand() % 2) dir.y = -dir.y;
+
+                directions.push_back(dir);
+
+                processedWindows.insert(hwnd);
+            }
+        }
+
+        LeaveCriticalSection(&cs);
+        return TRUE;
     }
+
+    bool IsSystemWindow(HWND hwnd) {
+        TCHAR className[256];
+        GetClassName(hwnd, className, 256);
+
+        if (lstrcmp(className, TEXT("Progman")) == 0 ||
+            lstrcmp(className, TEXT("Shell_TrayWnd")) == 0 ||
+            lstrcmp(className, TEXT("Button")) == 0) {
+            return true;
+        }
+
+        LONG style = GetWindowLong(hwnd, GWL_STYLE);
+        if (!(style & WS_OVERLAPPEDWINDOW) && !(style & WS_POPUP)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    void DiscoverExistingWindows() {
+        EnumWindows(EnumWindowsProc, reinterpret_cast<LPARAM>(this));
+    }
+
+    void DiscoverNewWindows() {
+        std::thread([this]() {
+            while (running) {
+                EnumWindows(EnumWindowsProc, reinterpret_cast<LPARAM>(this));
+                std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            }
+            }).detach();
+    }
+
+    void MoveWindows() {
+        while (running) {
+            EnterCriticalSection(&cs);
+
+            for (int i = windows.size() - 1; i >= 0; i--) {
+                if (!IsWindow(windows[i])) {
+                    windows.erase(windows.begin() + i);
+                    originalPositions.erase(originalPositions.begin() + i);
+                    originalSizes.erase(originalSizes.begin() + i);
+                    currentPositions.erase(currentPositions.begin() + i);
+                    directions.erase(directions.begin() + i);
+                }
+            }
+
+            for (size_t i = 0; i < windows.size(); i++) {
+                if (!IsWindowVisible(windows[i]) || GetParent(windows[i]) != NULL) {
+                    continue;
+                }
+
+                currentPositions[i].x += directions[i].x;
+                currentPositions[i].y += directions[i].y;
+
+                if (currentPositions[i].x <= 0) {
+                    currentPositions[i].x = 0;
+                    directions[i].x = -directions[i].x;
+                }
+                else if (currentPositions[i].x + originalSizes[i].cx >= screenW) {
+                    currentPositions[i].x = screenW - originalSizes[i].cx;
+                    directions[i].x = -directions[i].x;
+                }
+
+                if (currentPositions[i].y <= 0) {
+                    currentPositions[i].y = 0;
+                    directions[i].y = -directions[i].y;
+                }
+                else if (currentPositions[i].y + originalSizes[i].cy >= screenH) {
+                    currentPositions[i].y = screenH - originalSizes[i].cy;
+                    directions[i].y = -directions[i].y;
+                }
+
+                SetWindowPos(windows[i], 0,
+                    currentPositions[i].x,
+                    currentPositions[i].y,
+                    originalSizes[i].cx,
+                    originalSizes[i].cy,
+                    SWP_NOZORDER | SWP_NOACTIVATE);
+            }
+
+            LeaveCriticalSection(&cs);
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+    }
+
+    void Stop() {
+        running = false;
+    }
+};
+
+WindowManager* g_manager = nullptr;
+
+BOOL WINAPI ConsoleHandler(DWORD signal) {
+    if (signal == CTRL_C_EVENT) {
+        if (g_manager) {
+            g_manager->Stop();
+        }
+        return TRUE;
+    }
+    return FALSE;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    WindowManager manager;
+    g_manager = &manager;
+
+    SetConsoleCtrlHandler(ConsoleHandler, TRUE);
+
+    srand(static_cast<unsigned int>(GetTickCount()));
+
+    manager.DiscoverExistingWindows();
+    manager.DiscoverNewWindows();
+    manager.MoveWindows();
 
     return 0;
 }
